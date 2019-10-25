aeda459d5c4a1a6c0f4826d8979754d583087afd
Buddhiofficial <56988948+Buddhiofficial@users.noreply.github.com>
2019-10-25 09:51:33 +0530

    Update Array.c

--- Array.c
+++ Array.c
@@ -1,19 +1,19 @@
-void sum(int arr_1[],int arr_2[],int x)
+void sum(int Arr_1[],int Arr_2[],int x)
 {
 for(int a=0;a<x;a++)
 {
 printf("Enter value for Array 1 Element %d : ",a+1);
-scanf("%d",&arr_1[a]);
+scanf("%d",&Arr_1[a]);
 }
 for(int a=0;a<x;a++)
 {
 printf("Enter value for Array 2 Element %d : ",a+1);
-scanf("%d",&arr_2[a]);
+scanf("%d",&Arr_2[a]);
 }
 int sum[x];
 for(int c=0;c<x;c++)
 {
-sum[c] = arr_1[c]+arr_2[c];
+sum[c] = Arr_1[c]+Arr_2[c];
 printf("%d\t",sum[c]);
 }
 }
@@ -22,6 +22,6 @@ int main()
 int size;
 printf("Enter the size of Arrays : ");
 scanf("%d",&size);
-int arr_1[size],arr_2[size];
-sum(arr_1,arr_2,size);
+int Arr_1[size],Arr_2[size];
+sum(Arr_1,Arr_2,size);
 }
